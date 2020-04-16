1bd0cccea9ab2d2090260d83f6a1f58fcd69ddd2
Erick <egavin1992@gmail.com>
2020-04-16 16:48:13 -0400

    textures update

---
+++ textures/script.js
@@ -0,0 +1,11 @@
+
+const turbulence = document.querySelector("#turbulence")
+const lighting = document.querySelector("#lighting")
+const distant = document.querySelector("#distant")
+
+const list = [turbulence, lighting, distant]
+
+
+
+
+
